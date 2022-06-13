a2a93dfc72ae2e8c4576711b46ca6fd028d995e1
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2022-06-13 19:31:30 -0400

    desafio

---
+++ Sapiens.Aula6/Program.cs
@@ -0,0 +1,26 @@
+﻿using Sapiens.Core;
+
+Menu.DesenhaTitulo("Desafio de Programação");
+Desafio1();
+Console.ReadLine();
+
+// dado um vetor mostrar apenas os números pares do vetor
+void Desafio1()
+{
+    var vetor = Vetor.CriaAleatorio(20);
+    Console.WriteLine(vetor.Texto());
+    foreach (var numero in vetor)
+    {
+        if (numero % 2 == 0) // é par
+        {
+            Console.Write(numero + ", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void Desafio2()
+{
+
+}
+
